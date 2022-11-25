e62b4b6e883b8eefb9df8d1a60585dcb11009a57
MarcosAlexandreCoelhoCampos <marcooscampos@hotmail.com>
2022-11-25 17:54:38 -0300

    Header Component

---
+++ components/header/header.js
@@ -0,0 +1,36 @@
+export default function componentHeader() {
+  class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+      <header>
+        <div class="container header">
+          <a href="../index.html">
+            <h1>CanadaFly</h1>
+          </a>
+          <ul class="menu-desktop">
+            <li><a href=""> Canadá </a></li>
+            <li><a href=""> Imigração </a></li>
+            <li><a href=""> Serviços </a></li>
+            <li><a href=""> Sobre </a></li>
+          </ul>
+          <div class="menu-mobile">
+            <img src="../../imgs/menu-button.svg" alt="Botão de menu">
+            <ul>
+              <li><a href=""> Canadá </a></li>
+              <li><a href=""> Imigração </a></li>
+              <li><a href=""> Serviços </a></li>
+              <li><a href=""> Sobre </a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    `;
+    }
+  }
+  customElements.define("header-component", Header);
+}
+
+/* <img src="../../${this.attributes[0].value}" alt=""> */

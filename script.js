4d6737b7a3d230b10b2058e760bdd88fd4458954
MarcosAlexandreCoelhoCampos <marcooscampos@hotmail.com>
2022-11-30 14:47:29 -0300

    init dragabble slide

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ import componentHeader from "./components/header/header.js";
 componentHeader();
 import componentFooter from "./components/footer/footer.js";
 componentFooter();
+import dragabbleSlide from "./components/slide/slide.js";
+dragabbleSlide();
